15e677b8c526604e9bbc3238f3eb53f74de8b03b
Mkmuniz <mikael.muniz@bolha.com.br>
2025-02-01 23:52:17 -0300

    feat: create provider props

---
+++ front/src/types/providers.types.ts
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export type NextAuthSessionProviderProps = {
+    children: ReactNode;
+};
+
+export type TanstackProviderProps = {
+    children: ReactNode;
+};
